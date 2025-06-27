0ce6fe31a5021e2c7e6504b29a91723ed10012ce
albin Benny <albinbenny5122@gmail.com>
2025-06-27 15:59:14 -0400

    added jc-44

---
+++ frontend-jc/src/pages/Jobseeker/Dashboard.jsx
@@ -0,0 +1,13 @@
+// pages/Jobseeker/Dashboard.jsx
+import React from 'react';
+
+const JobseekerDashboard = () => {
+  return (
+    <div>
+      <h1>Welcome, Job Seeker!</h1>
+      <p>You can now browse and apply for jobs.</p>
+    </div>
+  );
+};
+
+export default JobseekerDashboard;
